6d313c15b93cdf538c08dadc6553b13dde4e3a30
Antanader <anthonyskeets@rocketmail.com>
2024-09-23 17:13:22 -0600

    Add files via upload

---
+++ CheckingAccount.java
@@ -0,0 +1,42 @@
+/*
+Name: Anthony Talavera
+Date: 9.20.2024
+Program: Checking Account Class
+ */
+
+class CheckingAccount extends BankAccount {
+    private double overdraft;
+
+
+    //Setting Checking Account
+    public CheckingAccount(String accountNumber, String accountHolderName, double balance) {
+        super(accountNumber, accountHolderName, balance);
+    }
+
+    // Getters and Setters
+    //Overdraft on Checking Account
+
+    //Get Account Info
+    public void getAccount() {
+        System.out.println("Account Number: " + getAccountNumber());
+        System.out.println("Account Name: " + getAccountName());
+        System.out.println("Balance: " + getBalance());
+    }
+
+
+    // Implement abstract methods
+
+    //deposit
+    public void deposit(double amount) {
+        setBalance(getBalance() + amount);
+    }
+
+    //withdraw with checking overdraft limit
+    public boolean withdraw(double amount) {
+        if (getBalance() >= amount) {
+            setBalance(getBalance() - amount);
+            return true;
+        }
+        return false;
+    }
+} //end
---
+++ SavingAccount.java
@@ -0,0 +1,55 @@
+/*
+Name: Anthony Talavera
+Date: 9.20.2024
+Program: Saving Account Class
+ */
+
+class SavingsAccount extends BankAccount {
+    private double interestRate;
+
+    // Saving Account
+    public SavingsAccount(String accountNumber, String accountHolderName, double balance, double interestRate) {
+        super(accountNumber, accountHolderName, balance);
+        this.interestRate = interestRate;
+    }
+
+    // Getters and Setters
+
+    //InterestRate
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    //SetInterestRate
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    //Get Account Info
+    public void getAccount(){
+        System.out.println("Account Number: " + getAccountNumber());
+        System.out.println("Account Name: " + getAccountName());
+        System.out.println("Balance: " + getBalance());
+        System.out.println("Interest Rate: " + getInterestRate());
+    }
+
+    // Implement abstract methods
+    //Deposit
+    public void deposit(double amount) {
+        setBalance(getBalance() + amount);
+    }
+
+    //Withdraw
+    public boolean withdraw(double amount) {
+        if (getBalance() >= amount) {
+            setBalance(getBalance() - amount);
+            return true;
+        }
+        return false;
+    }
+
+    // Applying Interest
+    public void applyInterest() {
+        setBalance(getBalance() + (getBalance() * interestRate));
+    }
+}
