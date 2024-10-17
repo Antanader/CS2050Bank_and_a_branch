f512acc01ca37b06c43b480e1332ac61612c27eb
Antanader <anthonyskeets@rocketmail.com>
2024-10-16 18:02:27 -0700

    Add files via upload

--- Account.java
+++ Account.java
@@ -4,6 +4,83 @@ Date: 9.20.2024
 Program: Bank Account Class
  */
 
+
+import java.io.Serializable;
+import java.util.Random;
+
+abstract class Account implements Serializable {
+    private static String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    // Bank Account Constructor
+    public Account(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    //
+    public Account(String accountHolderName){
+        this.accountNumber = String.valueOf(generateAccountNumber());
+        this.accountHolderName = accountHolderName;
+        this.balance = 1000.00;
+    }
+
+    // Account Number Generator
+    private static long generateAccountNumber() {
+        Random random = new Random();
+        return 100000000000L + (long) (random.nextDouble() * 900000000000L);
+    }
+
+    // Account Number Get
+    public static String getAccountNumber() {
+        return accountNumber;
+    }
+
+    // Set Account Number
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    // Get Account Name
+    public String getAccountName() {
+        return accountHolderName;
+    }
+
+    // Set Account Name
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    // Get Account Info
+    public void getAccount() {
+        System.out.println("Account Number: " + getAccountNumber());
+        System.out.println("Account Name: " + getAccountName());
+        System.out.println("Balance: " + getBalance());
+        System.out.println("\n");
+    }
+
+    // Balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Set Balance
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // Deposit
+    public abstract void deposit(double amount);
+
+    // Withdraw
+    public abstract boolean withdraw(double amount);
+}
+
+
+/*
+
 import java.util.Random;
 
 abstract class Account extends ProgramTest {
@@ -71,3 +148,4 @@ abstract class Account extends ProgramTest {
     // withdraw
     public abstract boolean withdraw(double amount);
 }
+*/
--- ProgramTest.java
+++ ProgramTest.java
@@ -61,6 +61,123 @@ public class ProgramTest {
         }
 
         //Customers Names:
+        CustomQueue queue = new CustomQueue();
+        Customer[] customers = new Customer[10];
+        File file = new File("accounts.txt");
+        try (Scanner scanner = new Scanner(file)){
+            while (scanner.hasNextLine()) {
+                String customerName = scanner.nextLine(); // Assuming the format: just the name
+
+                // Create a customer with just the name
+                Customer customer = new Customer(customerName);
+
+                // Enqueue the customer
+                queue.enqueue(customer);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+            return; // Exit the program if the file is not found
+        }
+
+        // Create a stack of accounts
+        Stack<Customer> accountStack = new Stack<>();
+        for (Customer customer : customers) {
+            accountStack.push(customer);
+        }
+
+        //Number Generator
+        Random random = new Random();
+
+        //30 loops
+
+        for (int i=0; i<30;i++){
+            Customer customer = queue.dequeue();
+            //Assign an account if they don't
+            if (Account.getAccountNumber() == null) {
+                Customer accountFromStack = accountStack.pop();
+                Objects.requireNonNull(customer).setAccountNumber(Account.getAccountNumber());
+                customer.setBalance(accountFromStack.getBalance());
+            }
+
+            double randomValue = random.nextInt(1000) - 500; // Values between -500 and 500
+
+            // check negative or positive
+            if (randomValue > 0) {  // Deposit for positive
+                Objects.requireNonNull(customer).deposit(randomValue);
+                System.out.println("Action: " + (i + 1) + " " + customer.getAccountName() + " deposits: $" + randomValue);
+            } else {  // Withdraw for negative or zero amounts
+                Objects.requireNonNull(customer).withdraw(-randomValue);  // Withdraw
+                System.out.println("Action: " + (i + 1) + " " + customer.getAccountName() + " withdraws: $" + -randomValue);
+            }
+            // Re-add the customer to the rear of the queue
+            queue.enqueue(customer);
+        }
+        // Print the final details of each customer
+        System.out.println("---------------------------");
+        System.out.println("\nFinal Customer Details:");
+        while (!queue.isEmpty()) { //if empty
+            Customer customer = queue.dequeue();
+            Objects.requireNonNull(customer).getAccount();
+        }
+
+
+
+
+/* Program 7 (old)
+public class ProgramTest {
+    public static void main(String[] args) {
+
+        // Node Class
+        class Node {
+            Customer customer;
+            Node next;
+
+            public Node(Customer customer) {
+                this.customer = customer;
+                this.next = null;
+            }
+        }
+
+        //Queue Implementation
+        class CustomQueue {
+            private Node front;
+            private Node rear;
+
+            public CustomQueue() {
+                this.front = null;
+                this.rear = null;
+            }
+            //Customer (end) queue
+            public void enqueue(Customer customer) {
+                Node newNode = new Node(customer);
+                if (rear == null) {
+                    front = rear = newNode;
+                } else {
+                    rear.next = newNode;
+                    rear = newNode;
+                }
+            }
+
+
+            //Customer (start) queue
+            public Customer dequeue() {
+                if (front == null) {
+                    return null;
+                }
+                Node temp = front;
+                front = front.next;
+                if (front == null) {
+                    rear = null;
+                }
+                return temp.customer;
+            }
+            //check if queue is empty
+            public boolean isEmpty() {
+                return front == null;
+            }
+        }
+
+        //Customers Names:
         String[] customerNames = {
                 "Osiris Wang", "Dorian Morris", "Presley Robles", "Collins Burke",
                 "Hayden Greene", "Malik Baxter", "Ryann Wiley", "Briggs Thomas",
@@ -109,12 +226,11 @@ public class ProgramTest {
         }
         // Print the final details of each customer
         System.out.println("\nFinal Customer Details:");
-        while (!queue.isEmpty()) {
+        while (!queue.isEmpty()) { //if empty
             Customer customer = queue.dequeue();
             Objects.requireNonNull(customer).getAccount();
         }
-
-
+ */
 
         /*
         //Program 6 - The Customers
